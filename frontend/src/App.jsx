cecf113b3759f4d41ddac721a5c43a073d290f07
blanklavender <rudrapatimahima248@gmail.com>
2025-04-19 21:48:42 -0700

    default background set

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import LoginPage from './pages/LoginPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className="text-center mt-10">
-        <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind!</h1>
-      </div>
+      <LoginPage/>
     </>
   )
 }
---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LoginPage = () => {
+  return (
+    <>
+    <div className='font-display'>
+      Hi, i am doing good
+    </div>
+    </>
+  );
+};
+
+export default LoginPage;
